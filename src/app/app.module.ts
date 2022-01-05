840d8e3ce6cfd310b3696ccbea3d7298a2d2d978
Benjamín Guzmán <bg@benjaminguzman.net>
2022-01-05 13:03:06 -0600

    Working on the landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import {AppComponent} from './app.component';
 import {NavComponent} from './nav/nav.component';
 import {FooterComponent} from './footer/footer.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {PortfolioComponent} from './portfolio/portfolio.component';
+import {ProjectComponent} from './portfolio/project/project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    PortfolioComponent,
+    ProjectComponent
   ],
   imports: [
     BrowserModule,
