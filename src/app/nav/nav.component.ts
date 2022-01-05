4013e878f0a008a3a3eb4917e0709afd29fd743f
Benjamín Guzmán <bg@benjaminguzman.net>
2022-01-05 14:21:59 -0600

    Added header animation

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,6 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {animate, state, style, transition, trigger} from "@angular/animations";
-
-export enum AnimationState {
-  TRANSPARENT = "transparent",
-  BLACK = "black"
-}
+import {environment} from "../../environments/environment";
 
 @Component({
   selector: 'app-nav',
@@ -12,6 +8,20 @@ export enum AnimationState {
   styleUrls: ['./nav.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   animations: [
+    trigger("headerAnimation", [
+      state("open", style({
+        top: "0"
+      })),
+      state("closed", style({
+        top: "-3.5rem"
+      })),
+      transition("open => closed", [
+        animate("400ms ease-in")
+      ]),
+      transition("closed => open", [
+        animate("400ms ease-out")
+      ])
+    ]),
     trigger("menuAnimation", [
       state("open", style({
         height: "100%",
@@ -45,9 +55,9 @@ export class NavComponent implements OnInit {
   public nextMenuStateIcon: 'menu' | 'close' = 'menu';
 
   /**
-   * Holds the name of the current state of the nav animation
+   * Holds the name of the current state of the header animation
    */
-  public navAnimationState: AnimationState = AnimationState.TRANSPARENT;
+  public headerAnimationState: 'open' | 'closed' = 'open';
 
   /**
    * Holds the name of the current state of the nav menu animation
@@ -59,10 +69,27 @@ export class NavComponent implements OnInit {
    */
   public isNavShown: boolean = false;
 
+  private lastScrollPos: number = 0;
+
   constructor(private changeDetectorRef: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.toggleDarkMode();
+
+    if (environment.useHeaderAnimation)
+      window.onscroll = () => this.headerAnimation();
+  }
+
+  public headerAnimation() {
+    const top = document.documentElement.scrollTop || document.body.scrollTop;
+
+    if (top > this.lastScrollPos) // going down
+      this.headerAnimationState = 'closed';
+    else // going up
+      this.headerAnimationState = 'open';
+
+    this.lastScrollPos = top;
+    this.changeDetectorRef.markForCheck();
   }
 
   public toggleDarkMode(): void {
@@ -80,7 +107,7 @@ export class NavComponent implements OnInit {
       document.documentElement.classList.remove('dark');
   }
 
-  public toggleNav() {
+  public toggleNavMenu() {
     if (this.isNavShown) {
       this.isNavShown = false;
       this.menuAnimationState = 'closed';
