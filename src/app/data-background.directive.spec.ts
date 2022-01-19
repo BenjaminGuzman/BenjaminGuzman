28bf5a77181974c693de983dd0d8796fdbef66c6
Benjamín Guzmán <bg@benjaminguzman.net>
2022-01-18 22:29:26 -0600

    Improvements to portfolio Added scaffolding for footer (contact info and contact form) Added lazy loading in project slideshow

---
+++ src/app/data-background.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DataBackgroundDirective } from './data-background.directive';
+
+describe('DataBackgroundDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DataBackgroundDirective();
+    expect(directive).toBeTruthy();
+  });
+});
