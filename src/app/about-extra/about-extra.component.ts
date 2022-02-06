7347f443c57c5ced044fbff5d4955e59f9a27142
Benjamín Guzmán <bg@benjaminguzman.net>
2022-02-06 12:51:13 -0600

    Changed detection strategy in some components

--- src/app/about-extra/about-extra.component.ts
+++ src/app/about-extra/about-extra.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-about-extra',
   templateUrl: './about-extra.component.html',
-  styleUrls: ['./about-extra.component.scss']
+  styleUrls: ['./about-extra.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AboutExtraComponent implements OnInit {
 
