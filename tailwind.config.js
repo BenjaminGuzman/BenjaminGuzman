a7fa4bbb7fdf04508e8157b94011cc0ea62d8332
Benjamín Guzmán <code@bguzman.dev>
2026-07-24 14:38:27 -0600

    improved aesthetics

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        obsidian: '#0b0b0b',
+      },
+      boxShadow: {
+        'neon': '0 0 15px rgba(34, 197, 94, 0.6)',
+      },
+      dropShadow: {
+        'neon': '0 0 10px rgba(34, 197, 94, 0.8)',
+      }
+    },
   },
   plugins: []
 }
